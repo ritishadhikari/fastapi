5b0720d141555d263d63c41881e8c2c4b353581d
Ritish Adhikari <ritishadhikari@gmail.com>
2025-05-15 10:39:20 +0530

    Till 05/15/2025 10:39:20

--- Udemy/FastAPI_Request_Method_Logic_3/books.py
+++ Udemy/FastAPI_Request_Method_Logic_3/books.py
@@ -2,6 +2,23 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
-@app.get("/api-endpoint")
-async def first_api():
-    return {"message":"Hello Eric"}
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+@app.get("/books")
+async def read_all_books():
+    # return {"message":"Hello Eric"}
+    return BOOKS
+
+@app.get("/books/{dynamic_params}")
+async def read_book(dynamic_param:str):
+    for book in BOOKS:
+        if book.get('title').casefold()==dynamic_param.casefold():
+            return {"message":dynamic_param}
+    return {"message":"Book not found"}
